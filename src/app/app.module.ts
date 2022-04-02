4db2d424367eec8116e0c8f1da1c822c54b50709
Charlotte Marion <marion.charlotte@orange.fr>
2022-04-02 11:26:39 +0200

    first commit for the new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddAlimentComponent } from './add-aliment/add-aliment.component';
+import { ListAlimentComponent } from './list-aliment/list-aliment.component';
+import { FormsModule } from '@angular/forms';
+import { AlimentService } from './aliment.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAlimentComponent,
+    ListAlimentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AlimentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
